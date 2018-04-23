a8ef8d021190cbdbaa84204349c4169ab9fd4634
Hui Lin <longqiman@gmail.com>
2018-04-23 13:55:58 -0500

    setup

---
+++ 2018_06_ShortCourse/00-course-setup.r
@@ -0,0 +1,21 @@
+### -----------------------------
+### workshop setup
+### Hui Lin
+### http://scientistcafe.com
+### -----------------------------
+
+
+# install packages from CRAN
+p_needed <- c("plyr", "dplyr", "caret","e1071","gridExtra","lattice","imputeMissings",
+              "RANN","corrplot","nnet","car","gpairs","reshape2","psych","tidyr",
+              "ggplot2","sparklyr","readr","mvtnorm","MASS","data.table","magrittr",
+              "shiny","metricsgraphics","DT","leaflet","dygraphs","xts","lubridate",
+              "highcharter","rbokeh","networkD3","threejs","knitr","formatR")
+
+packages <- rownames(installed.packages())
+p_to_install <- p_needed[!(p_needed %in% packages)]
+if (length(p_to_install) > 0) {
+  install.packages(p_to_install)
+}
+
+lapply(p_needed, require, character.only = TRUE)
